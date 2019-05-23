81459d309c2462f80fc7005c07f8aef9d426e0a4
stizzle123 <arifayanidowu2@yahoo.com>
2019-05-23 16:38:23 +0100

    Structured post-list interface and feel

--- client/src/app/posts/post-list/post-list.component.ts
+++ client/src/app/posts/post-list/post-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.scss"]
 })
 export class PostListComponent implements OnInit {
+  // posts = [
+  //   { title: "First Post", content: "This is the first post content" },
+  //   { title: "Second Post", content: "This is the second post content" },
+  //   { title: "Third Post", content: "This is the third post content" }
+  // ];
 
-  constructor() { }
+  posts = [];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
