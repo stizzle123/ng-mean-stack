1f2cbaeaf73a61bfd0cb36b780ded38bf53e5dc0
stizzle123 <arifayanidowu2@yahoo.com>
2019-05-23 12:46:45 +0100

    Generated post-list component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,8 @@ import {
   MatInputModule,
   MatCardModule,
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatExpansionModule
 } from "@angular/material";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -13,9 +14,15 @@ import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./header/header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,7 +31,8 @@ import { HeaderComponent } from "./header/header/header.component";
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
